c277e46baa5ae104f81438e84e65ecc4cb39cd90
Sudeep <sdpjswl1@gmail.com>
2022-01-28 04:20:42 +0530

    Updated project settings and fixed warnings

--- ImageToMp4Converter/AppDelegate.swift
+++ ImageToMp4Converter/AppDelegate.swift
@@ -3,24 +3,19 @@
 //  ImageToMp4Converter
 //
 //  Created by sudeep on 15/04/20.
-//  Copyright © 2020 sudeep. All rights reserved.
+//  Copyright © 2020 Word Rolls. All rights reserved.
 //
 
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
-  func applicationDidFinishLaunching(_ aNotification: Notification) {
-    // Insert code here to initialize your application
-  }
-
-  func applicationWillTerminate(_ aNotification: Notification) {
-    // Insert code here to tear down your application
-  }
-
-
+class AppDelegate: NSObject, NSApplicationDelegate
+{
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
 }
-
